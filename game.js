7858de591f41611f7de918d48920578401bd6c74
Ryan McBride <mr.ryan444@gmail.com>
2021-11-11 21:32:03 -0700

    Add functionality to 'change game' button

--- game.js
+++ game.js
@@ -132,6 +132,13 @@ class Game {
       this.computerWon = true;
       this.draw = false;
     }
+    console.log(humanSelection);
+    console.log(computerSelection);
+    console.log(this.player1.wins);
+    console.log(this.player2.wins);
+    console.log(this.humanWon);
+    console.log(this.computerWon);
+    console.log(this.draw);
   }
 
   resetPlayers() {
